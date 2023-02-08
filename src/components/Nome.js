a2e4bc0ab0029a79745d4edb8cd52a6b536d24b9
Isabel Pereira <isabelalvaro116@gmail.com>
2023-02-08 20:18:22 +0000

    components lesson done

--- src/components/Nome.js
+++ src/components/Nome.js
@@ -1,8 +1,9 @@
 
 //posso tbm desestruturar metendo function App({aluno})  e usar simplesmente aluno em vez de props.aluno
 function Nome({aluno,idade}){
-return(
-  <span>seja bem vindo:{aluno} - Idade: {idade} anos </span>
-  )
-}
+  return(
+    <span>Bem-vindo: {aluno} - idade: {idade}</span>
+    );
+  
+  }
 export default Nome;
